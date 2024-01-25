dd8eeaea28e1ca5fc3b3f8ba1040a87afc4502a0
Rohid <rohidul209@gmail.com>
2024-01-25 17:28:28 +0600

    editor footer

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "./footer";
 import Preview from "./preview";
 
 export default function EditorPage() {
   return (
     <div className="relative flex flex-1 flex-col overflow-auto">
       <Preview />
+      <Footer />
     </div>
   );
 }
